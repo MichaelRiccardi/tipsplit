db4c477990b0f7c35ebff894f489262a61bbe6bc
Michael Riccardi <michaelriccardi@ufl.edu>
2016-12-17 02:43:44 -0500

    Input validation

--- README.md
+++ README.md
@@ -12,8 +12,8 @@ The following **required** functionality is complete:
 * [X] User can enter a bill amount, choose a tip percentage, and submit the form to see the tip and total values.
 * [X] Tip percentage choices use a PHP loop to output three radio buttons.
 * [X] PHP code sets reasonable default values for the form.
-* [ ] PHP code confirms the presence and correct format of submitted values.
-* [ ] Page indicates any form errors which need to be fixed.
+* [X] PHP code confirms the presence and correct format of submitted values.
+* [X] Page indicates any form errors which need to be fixed.
 * [X] Submitted form values are retained when errors or results are shown.
 
 The following **optional** features are implemented:
--- index.php
+++ index.php
@@ -1,15 +1,47 @@
 <?php
 
+function getErrorMessage()
+{
+    $formSubmitted = $_SERVER['REQUEST_METHOD'] == 'POST';
+    $validSubtotal = isset($_POST["subtotal"]) && is_numeric($_POST["subtotal"]) && ($_POST["subtotal"] > 0);
+    $validTipPercentage = isset($_POST["tipPercentage"]) && is_numeric($_POST["tipPercentage"]);
+        
+    if(!$formSubmitted)
+    {
+        return "";
+    }
+    else if(!$validSubtotal && !$validTipPercentage)
+    {
+        return "Please specify a valid subtotal and tip percentage.";
+    }
+    else if(!$validSubtotal)
+    {
+        return "Please specify a valid subtotal.";
+    }
+    else if(!$validTipPercentage)
+    {
+        return "Please select a tip percentage.";
+    }
+    else
+    {
+        return false;
+    }
+}
+
 function validInputs()
 {
-    return true; /// TODO: Data validation
+    return (getErrorMessage() === false);
 }
 
 function outputSubtotal()
 {
     if(isset($_POST["subtotal"]))
     {
-        echo $_POST["subtotal"];
+        $validSubtotal = isset($_POST["subtotal"]) && is_numeric($_POST["subtotal"]) && ($_POST["subtotal"] > 0);
+        if($validSubtotal)
+        {
+            echo $_POST["subtotal"];
+        }
     }
 }
 
@@ -54,13 +86,14 @@ function outputTipAndTotal()
     <body>
         <form method="post">
             <h1>tipsplit</h1>
+            <h3><?php echo getErrorMessage() ?></h3>
 
-            <p>Bill subtotal:  $<input name="subtotal" type="number" step="any" value="<?php outputSubtotal() ?>"></p>
+            <p>Bill subtotal:  $<input name="subtotal" type="text" step="any" value="<?php outputSubtotal() ?>"></p>
 
             <p>Tip percentage: <?php outputTipPercentageRadioButtons([ 10, 15, 20 ], 15); ?>
             
             </p>
-                
+            
             <p><input type="submit" value="Submit"></p>
             
             <p><?php outputTipAndTotal(); ?></p>
