e8efaebeb8b5953939b8559710ba0a5d41f4f291
Michael Riccardi <michaelriccardi@ufl.edu>
2016-12-17 10:30:12 -0500

    Added split functionality

--- index.php
+++ index.php
@@ -15,11 +15,11 @@ require "output.php";
             <h1>tipsplit</h1>
             <h3><?php Output::errorMessage() ?></h3>
 
-            <p>Bill subtotal:  $<input name="subtotal" type="text" step="any" value="<?php Output::subtotal() ?>"></p>
+            <p>Bill subtotal:  $<input name="subtotal" type="number" step="any" value="<?php Output::subtotal() ?>"></p>
 
-            <p>Tip percentage: <?php Output::radioButtons([ 10, 15, 20 ], 15); ?>
-            
-            </p>
+            <p>Tip percentage: <?php Output::radioButtons([ 10, 15, 20 ], 15); ?></p>
+                
+            <p>Split between: <input name="split" type="text" value="<?php Output::split() ?>"> person(s)</p>
             
             <p><input type="submit" value="Submit"></p>
             
