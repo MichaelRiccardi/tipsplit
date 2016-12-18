3a00e4a09c05775d535031cb03eee141112cfe45
Michael Riccardi <michaelriccardi@ufl.edu>
2016-12-17 20:08:24 -0500

    Fixed display bug on small/mobile device screens

--- index.php
+++ index.php
@@ -10,16 +10,12 @@ require "output.php";
     <head>
         <title>tipsplit - PHP Tip Calculator</title>
         
+        <meta name="viewport" content="width=device-width, initial-scale=0.8, maximum-scale=0.8">
+        
         <!-- Bootstrap: Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-        <!-- Optional theme ->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous"> -->
         
-        <link rel="stylesheet" href="custom.css?<?php echo rand(); ?>">
-
-        <!-- Latest compiled and minified JavaScript ->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script-->
+        <link rel="stylesheet" href="custom.css">
         
         <script src="script.js"></script>
         
